30cdcb26a8d1a2a6a422cedcae9a559e23477cdf
Marcos Roberto Kubaszewski <marcos.roberto@digma.com.br>
2015-09-29 22:33:56 -0300

    Yii2 Lesson - 12 Creating Modules

---
+++ backend/modules/settings/views/companies/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Companies */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="companies-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'company_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_status')->dropDownList([ 'active' => 'Active', 'inactive' => 'Inactive', ], ['prompt' => '']) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
