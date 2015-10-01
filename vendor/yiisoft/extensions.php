1e626cfdd0dd6b5c460d81dc776ae313aa62dde2
Marcos Roberto Kubaszewski <marcos.roberto@digma.com.br>
2015-09-30 21:40:16 -0300

    Yii2 Lesson - 14 Auto Suggest DropDown Search

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -78,7 +78,7 @@ return array (
   'kartik-v/yii2-widget-select2' => 
   array (
     'name' => 'kartik-v/yii2-widget-select2',
-    'version' => '9999999-dev',
+    'version' => '2.0.3.0',
     'alias' => 
     array (
       '@kartik/select2' => $vendorDir . '/kartik-v/yii2-widget-select2',
