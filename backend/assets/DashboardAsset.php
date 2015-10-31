8180c4c778dc07b2e9736c8bb37333397e3fc08a
Marcos Roberto Kubaszewski <marcos.roberto@digma.com.br>
2015-10-30 23:19:50 -0200

    Yii2 Lesson - 39 Theme Integration AdminLTE part 2/2

--- backend/assets/DashboardAsset.php
+++ backend/assets/DashboardAsset.php
@@ -16,13 +16,25 @@ use yii\web\AssetBundle;
 class DashboardAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $baseUrl  = '@web';
     public $css = [
         'css/bootstrap.min.css',
+        '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
+        '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+        'css/AdminLTE.min.css',
+        'css/skins/_all-skins.min.css',
+        'css/plugins/iCheck/flat/blue.css',
         'css/site.css',
     ];
     public $js = [
-        'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
+        'js/bootstrap/bootstrap.min.js',
+        '//code.jquery.com/ui/1.11.4/jquery-ui.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
+        'plugins/sparkline/jquery.sparkline.min.js',
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/fastclick/fastclick.min.js',
+        'js/app.min.js',
+        'js/dashboard.js',
         'js/main.js'
     ];
     public $depends = [
